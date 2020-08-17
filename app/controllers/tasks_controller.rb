cff9fd2c1dc008e605243f4d65482fccdf669d5a
kudo.asato <asatosouta@gmail.com>
2020-08-17 09:54:53 +0000

    syuusei3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,7 @@ class TasksController < ApplicationController
     before_action :correct_user, only: [:destroy,:show,:update,:edit]
 
     def index
-        
-            @tasks = current_user.tasks
-            
-        
-        
+      @tasks = current_user.tasks
     end
     
   
@@ -28,11 +24,6 @@ class TasksController < ApplicationController
         end
         
     end
-    
-
-    
-    
-    
     
     def update
         
